4c2f35d9fc605d9002db2204757a599cb8e70915
Vitja <rymsha.viktor@gmail.com>
2017-08-26 20:31:04 +0300

    26.08

--- tasks_learn_JS/n14_factorial_fibonachi.js
+++ tasks_learn_JS/n14_factorial_fibonachi.js
@@ -1,38 +1,38 @@
-function factorial(n){
-    if (n!= 1) {
-        return n* factorial(n - 1);
+function factorial(n) {
+    if (n != 1) {
+        return n * factorial(n - 1);
     } else {
         return n
     }
 }
-  n = prompt("Вводи какого числа факториал посчитать")
+n = prompt("Вводи какого числа факториал посчитать")
 alert(factorial(n));
 
 
 // число Фибоначчи
 
-function fib(n){
-  
-    return n<= 1 ? n : fib(n-1) + fib(n - 2);
-    } 
+function fib(n) {
+
+    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
 
 
-n = prompt ('Ввести число');
+n = prompt('Ввести число');
 alert(fib(n));
 
 
-function fib(n){
+function fib(n) {
     var a = 1,
-    b = 1;
-  for (var i = 3; i <= n; i++) {
- 
-    var c = a + b;
-      
-    a = b;
-    b = c; 
-    
-  }
-  return b;
+        b = 1;
+    for (var i = 3; i <= n; i++) {
+
+        var c = a + b;
+
+        a = b;
+        b = c;
+
+    }
+    return b;
 }
-n = prompt ('Ввести число');
+n = prompt('Ввести число');
 alert(fib(n));
